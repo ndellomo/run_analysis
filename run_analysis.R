8d916fccb7ad068563f0ef5ad25972f0d73aa746
Nicholas Dellomo <dell'omo_nicholas@bah.com>
2014-05-25 12:49:20 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -28,9 +28,8 @@ colnames(all.activity)[1] <- "Activity"
 ## Appropriately label the data set with descriptive activity names
 activity_lables[,2]<-as.character(activity_lables[,2])
 
-for(i in 1:length(all.activity[,1])){
-      all.activity[i,1]<-activity_lables[all.activity[i,1],2]
-}
+for(i in 1:length(all.activity[,1]))
+{all.activity[i,1]<-activity_lables[all.activity[i,1],2]}
 
 ## Creates a second tidy data set with the meand of each variable for each activity and subject.
 Subject_all<-rbind(Subject_train,Subject_test)
@@ -40,9 +39,8 @@ colnames(all)[1] <- "Subject"
 ## Use the aggregate function to find the means
 Tidy <- aggregate( all[,3] ~ Subject+Activity, data = all, FUN= "mean" )
 
-for(i in 4:ncol(all)){
-      Tidy[,i] <- aggregate( all[,i] ~ Subject+Activity, data = all, FUN= "mean" )[,3]
-}
+for(i in 4:ncol(all))
+{Tidy[,i] <- aggregate( all[,i] ~ Subject+Activity, data = all, FUN= "mean" )[,3]}
 
 colnames(Tidy)[3:ncol(Tidy)] <- colnames(MeanSD)
 
